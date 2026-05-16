57ebfa4732f25bd4351fa58a02e8b042cf78fbf1
DanilRozhin <danilrozhin2005@mail.ru>
2026-05-17 01:51:12 +0300

    feat: stats schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,13 +1,18 @@
 from .device_schemas import DeviceCreate, DeviceResponse
-from .reading_schemas import ReadingBase, ReadingCreate, ReadingResponse
+from .reading_schemas import ReadingBase, ReadingCreate, ReadingResponse, ReadingResponseList
+from .stats_schemas import DeviceStatsResponse, StatsResponse, UserAggregatedStatsResponse
 from .user_schemas import UserCreate, UserResponse
 
 __all__ = [
     "DeviceCreate",
     "DeviceResponse",
+    "DeviceStatsResponse",
     "ReadingBase",
     "ReadingCreate",
     "ReadingResponse",
+    "ReadingResponseList",
+    "StatsResponse",
+    "UserAggregatedStatsResponse",
     "UserCreate",
     "UserResponse",
 ]
